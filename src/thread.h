ad278bbbc6f96f882fad870ef5165849c438dd1f
Wilson Kazuo Mizutani <kazuo256@gmail.com>
2012-05-02 15:35:38 -0300

    Removed TR1 dependencies.

--- src/thread.h
+++ src/thread.h
@@ -2,8 +2,6 @@
 #ifndef EP2_THREAD_H_
 #define EP2_THREAD_H_
 
-#include <tr1/memory>
-
 #include <pthread.h>
 
 namespace ep2 {
@@ -13,16 +11,12 @@ bool init_threads ();
 class Thread {
   public:
     typedef void* (*Routine) (void*);
-    typedef std::tr1::shared_ptr<Thread>      Ptr;
-    static Ptr create (Routine routine) {
-      return Ptr(new Thread(routine));
-    }
+    Thread (Routine routine) : routine_(routine) {}
     void  run (void *arg);
     void* join ();
   private:
     Routine   routine_;
     pthread_t thread_;
-    explicit Thread (Routine routine) : routine_(routine) {}
 };
 
 } // namespace ep2
