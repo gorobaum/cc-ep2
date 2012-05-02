03b6d156c575615570904564ce44c307152e4bfd
Thiago de Gouveia Nunes <gorobaum@gmail.com>
2012-05-02 14:41:46 -0300

    Iniciando a criação do grafo.

--- src/ep2.cxx
+++ src/ep2.cxx
@@ -1,9 +1,36 @@
+#include <cstdio>
+#include <cstring>
 #include "ep2.h"
 
 namespace ep2 {
  
 bool init (int argc, char** argv) {
-  return false; 
+  FILE *pFileIn;
+  //Grafo grafo;
+  
+  if (argc < 2) {
+    printf("você precisa passar o nome de um arquivo como entrada do programa.1\n");   
+    return false;
+  }
+  if (strcmp(argv[1], "-debug") == 0) {
+    printf("Modo debug ativado.\n");
+    argc--;
+    if (argv[2] == NULL) {
+      printf("você precisa passar o nome de um arquivo como entrada do programa.2\n");
+      return false;
+    }
+    strcpy(argv[1], argv[2]);
+  }
+  pFileIn = fopen(argv[1], "r");
+
+  if (pFileIn == NULL) {
+    printf("você precisa passar o nome de um arquivo como entrada do programa.3\n");
+    return false;
+  }
+
+  //grafo(pFilein);
+  
+  return true;
 }  
 
 }
--- src/main.cxx
+++ src/main.cxx
@@ -1,8 +1,11 @@
 #include <cstdio>
 #include "ep2.h"
+#include "thread.h"
 
 int main(int argc, char** argv) {
   if (!ep2::init(argc, argv)) 
     printf("KEDE O GRAFO? KEDE\n");
+  if (!ep2::init_threads()) 
+    printf("KEDE AS THREADS? KEDE\n");
   return 0;
 }
