e2e2cbc891c339014efbda1d32121e26ae41f659
Wilson Kazuo Mizutani <kazuo256@gmail.com>
2012-05-03 10:50:39 -0300

    Mutex and locks implemented.

--- src/mutex.cxx
+++ src/mutex.cxx
@@ -14,5 +14,16 @@ Mutex::~Mutex () {
     Log().warning("Mutex may not have been correctly destroyed.");
 }
 
+Mutex::Lock::Lock (Mutex& mutex) :
+  mutex_ref_(mutex) {
+  if (pthread_mutex_lock(&mutex_ref_.mutex_))
+    Log().warning("Mutex lock failed.");
+}
+
+Mutex::Lock::~Lock () {
+  if (pthread_mutex_unlock(&mutex_ref_.mutex_))
+    Log().warning("Mutex unlock failed.");
+}
+
 } // namespace ep2
 
--- src/mutex.h
+++ src/mutex.h
@@ -8,9 +8,19 @@ namespace ep2 {
 
 class Mutex {
   public:
+    class Lock {
+      public:
+        Lock (Mutex& mutex);
+        ~Lock ();
+      private:
+        Lock (const Lock&);
+        Lock& operator = (const Lock&);
+        Mutex &mutex_ref_;
+    };
     Mutex ();
     ~Mutex ();
   private:
+    friend class Lock;
     pthread_mutex_t mutex_;
 };
 
