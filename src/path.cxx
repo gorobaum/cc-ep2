4943ffe8d40d36ab735a08c8fa570462f2d5be66
Wilson Kazuo Mizutani <kazuo256@gmail.com>
2012-05-10 19:13:09 -0300

    Fixed path output.

--- src/path.cxx
+++ src/path.cxx
@@ -8,11 +8,17 @@ void Path::add_vertex (node vertex) {
   path_.push_back(vertex);
 }
 
+const char *seps[2] = {
+  " - ",
+  ""
+};
+
 void Path::dump () const {
   Path::VertexPath::const_iterator it;
 
+  printf("(%u) ", path_.size()-1);
   for (it = path_.begin(); it < path_.end(); it++)
-    printf("%u-", (*it));
+    printf("%u%s", (*it), seps[it+1==path_.end()]);
   printf("\n");
 }
 
