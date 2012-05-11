a0f5d05c9058170e5c3d233e4c3325a946f3fa3a
Wilson Kazuo Mizutani <kazuo256@gmail.com>
2012-05-11 16:28:08 -0300

    Optmizied path, needs compiling and testing.

--- src/path.cxx
+++ src/path.cxx
@@ -8,22 +8,12 @@ namespace ep2 {
 
 using std::string;
 
-void Path::add_vertex (node vertex) {
-  path_.push_back(vertex);
+Path Path::operator+ (node vertex) const {
+  return Path(new Link(link_), vertex);
 }
 
-bool Path::search_for_vertex (node vertex) {
-  Path::VertexPath::iterator it;
-  for (it = path_.begin(); it < path_.end(); it++)
-    if ((*it) == vertex) return true;
-  return false; 
-}
-
-bool Path::valid () const {
-  Path::VertexPath::const_iterator it;
-  for (it = path_.begin(); it+1 < path_.end(); it++)
-    if ((*it) == path_.back()) return false;
-  return true;
+bool Path::search_for_vertex (node vertex) const {
+  return link_->has(vertex);
 }
 
 const char *seps[2] = {
@@ -37,10 +27,9 @@ void Path::dump () const {
 
 Path::operator string () const {
   string pathstring;
-  pathstring += "(" + ep2::utos(path_.size()-1) + ") ";
-  Path::VertexPath::const_iterator it;
-  for (it = path_.begin(); it < path_.end(); it++)
-    pathstring += utos(*it) + seps[it+1==path_.end()];
+  for (Link *it = link_; it; it = it->parent())
+    pathstring = seps[!it->parent()] + utos(it->last()) + pathstring;
+  pathstring = "(" + ep2::utos(path_.size()) + ") " + pathstring;
   return pathstring;
 }
 
--- src/path.h
+++ src/path.h
@@ -10,23 +10,40 @@ namespace ep2 {
 
 class Path {
   public:
-    typedef std::vector<node> VertexPath;
+    typedef std::vector<bool> VertexSet;
 
-    explicit Path (node initial_vertex) {
-      path_.push_back(initial_vertex);
-    }
+    Path (size_t n) : link_(new Link(n)) {}
 
-    void add_vertex (node vertex);
-    bool search_for_vertex (node vertex);
-    bool valid () const;
-    node last () const { return path_.back(); } 
+    Path operator+ (node vertex) const;
+    bool search_for_vertex (node vertex) const;
+    node last () const { return link_->last(); } 
     void dump () const;
     bool operator< (const Path& rhs) const {
       return path_.size() < rhs.path_.size();
     }
     operator std::string () const;
   private:
-    VertexPath  path_;
+    class Link {
+        Link (size_t n) :
+          last_(0), parent_(NULL), set_(n, false), size_(0) {
+          set_[last_] = true;
+        }
+        Link (const Link *parent, node last) :
+          last_(last), parent_(parent), set_(parent->set_), size_(parent->size_+1) {
+          set_[last_] = true;
+        }
+        const Link* parent () const { return parent_; }
+        node last () const { return last; }
+        size_t size() const { return size_; }
+        bool has (node vertex) const { return set_[vertex]; }
+      private:
+        const node    last_;
+        const Link    *parent_;
+        VertexSet     set_;
+        const size_t  size_;
+    };
+    Link *link_;
+    explicit Path (Link *link) : link_(link) {}
 };
 
 }
